60b8100d7c00d5c569dfa43d6be39bea039e9d0a
Aleeza <malikaleeza886@gmail.com>
2024-01-21 08:45:58 -0800

    done :)

--- src/components/ErrorScreen.js
+++ src/components/ErrorScreen.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ErrorScreen = () => {
-  return <div>ErrorScreen</div>;
+  return <>
+  <div className="bg-white text-black p-3 rounded">
+    💥 There was an error fecthing questions.
+    </div>
+  </>;
 };
 
 export default ErrorScreen;
--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,7 +1,22 @@
 import React from "react";
+import {Blocks} from "react-loader-spinner";
 
 const LoadingScreen = () => {
-  return <div>LoadingScreen</div>;
+  return (
+    <>
+      <div className='mx-auto spinner'>
+        <Blocks
+          height='80'
+          width='80'
+          color='#020035'
+          ariaLabel='blocks-loading'
+          wrapperStyle={{}}
+          wrapperClass='blocks-wrapper'
+          visible={true}
+        />
+      </div>
+    </>
+  );
 };
 
 export default LoadingScreen;
